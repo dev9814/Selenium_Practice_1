86a1bf860f3a1056e3fc7b01d05048c2583d1e3c
dev9814 <devdutt.software@gmail.com>
2024-06-10 20:13:16 +0530

    adding some assertion files

---
+++ src/test/java/assertions/Assert_1.java
@@ -0,0 +1,16 @@
+package assertions;
+
+public class Assert_1 {
+
+	public static void main(String[] args) {
+		
+		String[] weeks = {"Friday", "Saturday", "Sunday"};
+		assert weeks.length == 2;
+		System.out.println("we have "+ weeks.length + " weekend days in a week");
+		
+		
+		
+
+	}
+
+}
